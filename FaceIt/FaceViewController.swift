df75bfe64991b1ca8c914eeba83c71c4666a1105
Shaolong <shaolonglin09@gmail.com>
2016-07-12 19:09:34 -0400

    Example Multiple View project
    CS 193p

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Shaolong Lin on 7/7/16.
+//  Copyright © 2016 Shaolong Lin. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+	var expression = FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile) {
+		didSet {
+			updateUI()
+		}
+	}
+	
+	@IBOutlet weak var faceView: FaceView! {
+		didSet { // when this outlet is wired up, it will be called
+			faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: #selector(FaceView.changeScale(_:))))
+			let happierSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(FaceViewController.increaseHappiness))
+			happierSwipeGestureRecognizer.direction = .Up
+			faceView.addGestureRecognizer(happierSwipeGestureRecognizer)
+			let sadderSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action:#selector( FaceViewController.decreaseHappiness))
+			sadderSwipeGestureRecognizer.direction = .Down
+			faceView.addGestureRecognizer(sadderSwipeGestureRecognizer)
+			
+			updateUI()
+		}
+	}
+	@IBAction func toggleEyes(recognizer: UITapGestureRecognizer) {
+		if recognizer.state == .Ended {
+			switch expression.eyes {
+			case .Open:
+				expression.eyes = .Closed
+			case .Closed:
+				expression.eyes = .Open
+			case .Squinting: break
+				
+			}
+		}
+	}
+	
+	func decreaseHappiness() {
+		expression.mouth = expression.mouth.sadderMouth()
+	}
+	
+	func increaseHappiness() {
+		expression.mouth = expression.mouth.happierMouth()
+	}
+	
+	private var mouthCurvatures = [
+		FacialExpression.Mouth.Frown: -1.0,
+		.Grin: 0.5,
+		.Smile: 1.0,
+		.Smirk: -0.5,
+		.Neutral: 0.0
+	]
+	
+	private var eyeBrowTilts = [
+		FacialExpression.EyeBrows.Relaxed: 0.5,
+		.Furrowed: -0.5,
+		.Normal: 0.0
+	]
+	
+	private func updateUI() {
+		if faceView != nil {
+			switch expression.eyes {
+			case .Open:
+				faceView.eyesOpen = true
+			case .Closed:
+				faceView.eyesOpen = false
+			case .Squinting:
+				faceView.eyesOpen = true
+			}
+			faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+			faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
+
+		}
+	}
+}
+
